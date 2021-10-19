4ba8c0ae827c21553d4a830e6b6e38147b7967d1
Joost Meijles <joost@meijles.com>
2021-10-19 19:59:33 +0200

    Add example

---
+++ src/app.tsx
@@ -0,0 +1,32 @@
+import { Logo } from './logo'
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+
+export function App() {
+  const [ref] = useKeenSlider<HTMLDivElement>()
+
+  return (
+    <>
+      <div ref={ref} className="keen-slider">
+        <div className="keen-slider__slide number-slide1">1</div>
+        <div className="keen-slider__slide number-slide2">2</div>
+        <div className="keen-slider__slide number-slide3">3</div>
+        <div className="keen-slider__slide number-slide4">4</div>
+        <div className="keen-slider__slide number-slide5">5</div>
+        <div className="keen-slider__slide number-slide6">6</div>
+      </div>
+      <Logo />
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  )
+}
